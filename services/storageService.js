9e8cee8796fd5109cff584b1aa5cd1811ab717f0
Leo <leokaique7@gmail.com>
2025-04-28 11:39:04 -0300

    ajustes

--- services/storageService.js
+++ services/storageService.js
@@ -1,13 +1,17 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const getStoredData = async () => {
-  const user = await AsyncStorage.getItem("user");
-  const empresaNome = await AsyncStorage.getItem("empresaNome");
-  const filialNome = await AsyncStorage.getItem("filialNome");
+  const user = await AsyncStorage.getItem('user')
+  const empresaNome = await AsyncStorage.getItem('empresaNome')
+  const filialNome = await AsyncStorage.getItem('filialNome')
+
+  console.log('Stored user:', user)
+  console.log('Stored empresaNome:', empresaNome)
+  console.log('Stored filialNome:', filialNome)
 
   return {
     user: user ? JSON.parse(user) : null,
     empresaNome,
     filialNome,
-  };
-};
+  }
+}
