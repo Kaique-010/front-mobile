2d74a7bec8300accf9ae4f5c102a244882472d11
Leo <leokaique7@gmail.com>
2025-12-16 10:13:58 -0300

    ajustes gerais e de zoom

--- screens/Home.js
+++ screens/Home.js
@@ -208,7 +208,7 @@ export default function Home() {
           textTransform: 'uppercase',
           fontSize: 6,
         }}>
-        Versão 1.1.0
+        Versão 1.1.1
       </Text>
 
       <TouchableOpacity
